81e29b58a91947f54940c38ce5de23a391090e89
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2020-11-02 12:05:26 +0100

    clean up assessment

--- cmd/assessment/main.go
+++ cmd/assessment/main.go
@@ -3,7 +3,5 @@ package main
 import "clouditor.io/riskAssessment/internal/assessment"
 
 func main() {
-	// assessment.EvaluateExamplePolicy("resources/example_policy.rego")
-	// assessment.EvaluatePolicy("resources/testPolicy.txt", "resources/testTemplate.json")
 	assessment.EvaluatePolicy()
 }
--- internal/assessment/assessment.go
+++ internal/assessment/assessment.go
@@ -39,48 +39,6 @@ func EvaluatePolicy() {
 	}
 }
 
-func EvaluateExamplePolicy(policyPath string) {
-
-	// get policy
-	content, err := ioutil.ReadFile(policyPath)
-	module := string(content)
-	ctx := context.TODO()
-	query, err := rego.New(
-		rego.Query("x = data.example.authz.allow"),
-		rego.Module("example.rego", module),
-	).PrepareForEval(ctx)
-
-	if err != nil {
-		// Handle error.
-	}
-
-	input := map[string]interface{}{
-		"method": "GET",
-		"path":   []interface{}{"salary", "bob"},
-		"subject": map[string]interface{}{
-			"user":   "bob",
-			"groups": []interface{}{"sales", "marketing"},
-		},
-	}
-
-	results, err := query.Eval(ctx, rego.EvalInput(input))
-
-	for index, element := range results {
-		fmt.Println(element)
-		if err != nil {
-			// Handle evaluation error.
-		} else if len(results) == 0 {
-			// Handle undefined result.
-		} else if result, ok := results[index].Bindings["x"].(bool); !ok {
-			fmt.Println(result)
-		} else {
-			// Handle result/decision.
-			// fmt.Printf("%+v", results) => [{Expressions:[true] Bindings:map[x:true]}]
-		}
-	}
-
-}
-
 func jsonFileInput(path string) interface{} {
 
 	bs, err := ioutil.ReadFile(path)
